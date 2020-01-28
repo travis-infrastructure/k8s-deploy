b65cb663332e287ee0d57f929f4fb908628ccc58
Arek Rus <arek@travis-ci.org>
2020-01-28 21:28:05 +0100

    Add exit codes and build url to post data

--- flux-release.sh
+++ flux-release.sh
@@ -5,6 +5,7 @@ APP_NAME=$(cut -d "/" -f 2 <<< "${K8S_APP_REPO}")
 HELM_RELEASE="helmrelease/${DEPLOYMENT_NAME}"
 DOCKER_IMAGE_REPO="gcr.io/travis-ci-${PROJECT}-services-1/${APP_NAME}"
 VERSION_VALUE=$(git --git-dir=/${TRAVIS_BUILD_DIR}/src/.git describe --always --tags 2>/dev/null)
+NOTIFICATION_DATA='{"build_url":"'${TRAVIS_BUILD_WEB_URL}'"}'
 
 docker pull $DOCKER_IMAGE_REPO:$VERSION_VALUE
 
@@ -13,7 +14,9 @@ fluxctl --k8s-fwd-ns=$FLUX_NAMESPACE release \
           --update-image=$DOCKER_IMAGE_REPO:$VERSION_VALUE
 
 if [ "$?" -eq "0" ]; then
-  curl -X POST https://webhook.site/7117a92b-f063-448c-87d5-6c2b23c39a12/hubot/$PROJECT/$K8S_APP_REPO/$K8S_APP_REPO_COMMIT/success
+  curl -k -H "Content-Type: application/json" -X POST -d $NOTIFICATION_DATA https://fluxbot-staging.travis-ci.org/hubot/$PROJECT/$K8S_APP_REPO/$K8S_APP_REPO_COMMIT/success
+  exit 0
 else
-  curl -X POST https://webhook.site/7117a92b-f063-448c-87d5-6c2b23c39a12/hubot/$PROJECT/$K8S_APP_REPO/$K8S_APP_REPO_COMMIT/failed
+  curl -k -H "Content-Type: application/json" -X POST -d $NOTIFICATION_DATA https://fluxbot-staging.travis-ci.org/hubot/$PROJECT/$K8S_APP_REPO/$K8S_APP_REPO_COMMIT/failed
+  exit 1
 fi

91202f99ba53504fd2ff6848850898e40dcfd4e4
Arek <arek@travis-ci.org>
2020-03-13 12:32:59 +0100

    GKE cluster name

--- docker-build.sh
+++ docker-build.sh
@@ -6,7 +6,7 @@
 
 APP_NAME=$(cut -d "/" -f 2 <<< $K8S_APP_REPO)
 COMMIT_SHA_SHORT=$(git describe --always --tags 2>/dev/null)
-DOCKER_IMAGE_PATH="gcr.io/travis-ci-${PROJECT}-services-1/${APP_NAME}"
+DOCKER_IMAGE_PATH="gcr.io/${GCE_PROJECT}/${APP_NAME}"
 DOCKER_IMAGE_TAG=$(gcloud container images list-tags ${DOCKER_IMAGE_PATH} --filter="tags=${COMMIT_SHA_SHORT}" --format=json)
 
 if [[ "${DOCKER_IMAGE_TAG}" == "[]" ]]; then
--- flux-release.sh
+++ flux-release.sh
@@ -3,7 +3,7 @@
 FLUX_NAMESPACE="flux"
 APP_NAME=$(cut -d "/" -f 2 <<< "${K8S_APP_REPO}")
 HELM_RELEASE="helmrelease/${DEPLOYMENT_NAME}"
-DOCKER_IMAGE_REPO="gcr.io/travis-ci-${PROJECT}-services-1/${APP_NAME}"
+DOCKER_IMAGE_REPO="gcr.io/${GCE_PROJECT}/${APP_NAME}"
 VERSION_VALUE=$(git --git-dir=/${TRAVIS_BUILD_DIR}/src/.git describe --always --tags 2>/dev/null)
 NOTIFICATION_DATA='{"build_url":"'${TRAVIS_BUILD_WEB_URL}'"}'
 

807e64f48bd99668f361adf2b72f2b67a65934fa
Arek <arek@travis-ci.org>
2020-04-24 20:10:54 +0200

    Revert "notifcation depending on make status"
    This reverts commit a798faa40299a46a3fe4bbf7cc23b866ab511602.

--- flux-release.sh
+++ flux-release.sh
@@ -5,6 +5,7 @@ APP_NAME=$(cut -d "/" -f 2 <<< "${K8S_APP_REPO}")
 HELM_RELEASE="helmrelease/${DEPLOYMENT_NAME}"
 DOCKER_IMAGE_REPO="gcr.io/${GCE_PROJECT}/${APP_NAME}"
 VERSION_VALUE=$(git --git-dir=/${TRAVIS_BUILD_DIR}/src/.git describe --always --tags 2>/dev/null)
+NOTIFICATION_DATA='{"build_url":"'${TRAVIS_BUILD_WEB_URL}'"}'
 
 docker pull $DOCKER_IMAGE_REPO:$VERSION_VALUE
 
@@ -28,3 +29,10 @@ fluxctl --k8s-fwd-ns=$FLUX_NAMESPACE release \
           --namespace $NS \
           --update-image=$DOCKER_IMAGE_REPO:$VERSION_VALUE
 
+if [ "$?" -eq "0" ]; then
+  curl -k -H "Content-Type: application/json" -X POST -d $NOTIFICATION_DATA https://fluxbot-staging.travis-ci.org/hubot/$PROJECT/$K8S_APP_REPO/$K8S_APP_REPO_COMMIT/success
+  exit 0
+else
+  curl -k -H "Content-Type: application/json" -X POST -d $NOTIFICATION_DATA https://fluxbot-staging.travis-ci.org/hubot/$PROJECT/$K8S_APP_REPO/$K8S_APP_REPO_COMMIT/failed
+  exit 1
+fi

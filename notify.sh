b4d6db45be0041c8c50fee661e3e76b336e9b0fe
Arek <arek@travis-ci.org>
2020-04-24 20:10:40 +0200

    revert breaking changes

--- notify.sh
+++
@@ -1,9 +0,0 @@
-NOTIFICATION_DATA='{"build_url":"'${TRAVIS_BUILD_WEB_URL}'"}'
-
-if [ "$@" == 0 ]; then
-    curl -k -H "Content-Type: application/json" -X POST -d $NOTIFICATION_DATA https://fluxbot-staging.travis-ci.org/hubot/$PROJECT/$K8S_APP_REPO/$K8S_APP_REPO_COMMIT/success;
-    exit 0;
-else
-    curl -k -H "Content-Type: application/json" -X POST -d $NOTIFICATION_DATA https://fluxbot-staging.travis-ci.org/hubot/$PROJECT/$K8S_APP_REPO/$K8S_APP_REPO_COMMIT/failed;
-    exit 1;
-fi

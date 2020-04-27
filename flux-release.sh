ed7b1de0af49ea3d823354b68f78249583cd3fa7
Arek <arek@travis-ci.org>
2020-04-27 11:54:28 +0200

    revert notification

--- flux-release.sh
+++ flux-release.sh
@@ -28,3 +28,11 @@ fluxctl --k8s-fwd-ns=$FLUX_NAMESPACE release \
           --workload $WORKLOAD:$HELM_RELEASE \
           --namespace $NS \
           --update-image=$DOCKER_IMAGE_REPO:$VERSION_VALUE
+
+if [ "$?" -eq "0" ]; then
+  curl -k -H "Content-Type: application/json" -X POST -d $NOTIFICATION_DATA https://fluxbot-staging.travis-ci.org/hubot/$PROJECT/$K8S_APP_REPO/$K8S_APP_REPO_COMMIT/success
+  exit 0
+else
+  curl -k -H "Content-Type: application/json" -X POST -d $NOTIFICATION_DATA https://fluxbot-staging.travis-ci.org/hubot/$PROJECT/$K8S_APP_REPO/$K8S_APP_REPO_COMMIT/failed
+  exit 1
+fi 

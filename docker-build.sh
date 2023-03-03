7d2aff03b9929f61a69a850a3443b8d75f6ea184
Cesar Souto <cesar@travis-ci.org>
2023-03-03 11:22:48 -0300

    Wait for indexing

--- docker-build.sh
+++ docker-build.sh
@@ -19,7 +19,8 @@ if [[ "${DOCKER_IMAGE_TAG}" == "[]" ]]; then
   docker tag $DOCKER_IMAGE_PATH:$COMMIT_SHA_SHORT $DOCKER_IMAGE_PATH:latest
   gcloud docker -- push $DOCKER_IMAGE_PATH:$COMMIT_SHA_SHORT
   gcloud docker -- push $DOCKER_IMAGE_PATH:latest
-
+  echo "Wait a little bit for the indexing"
+  sleep 60
 else
   echo "Image already exist in gcr.io"
   exit 0

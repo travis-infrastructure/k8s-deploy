d20adc4a2745985201ce88bd72fd204d2b9cfbac
Arek Rus <57007917+r-arek@users.noreply.github.com>
2023-06-28 14:10:01 +0200

    Update docker-build.sh

--- docker-build.sh
+++ docker-build.sh
@@ -13,7 +13,7 @@ COMMIT_SHA_SHORT=$(git rev-parse --short HEAD 2>/dev/null)
 DOCKER_IMAGE_PATH="gcr.io/${GCE_PROJECT}/${ENV}/${APP_NAME}"
 
 echo "Building image..."
-#gcloud docker -- pull $DOCKER_IMAGE_PATH:latest || true
+gcloud docker -- pull $DOCKER_IMAGE_PATH:latest || true
 docker build . --build-arg bundle_gems__contribsys__com --cache-from $DOCKER_IMAGE_PATH:latest -t $DOCKER_IMAGE_PATH:build-${TRAVIS_BUILD_NUMBER}-$COMMIT_SHA_SHORT
 docker tag $DOCKER_IMAGE_PATH:build-${TRAVIS_BUILD_NUMBER}-$COMMIT_SHA_SHORT $DOCKER_IMAGE_PATH:latest
 gcloud docker -- push $DOCKER_IMAGE_PATH:build-${TRAVIS_BUILD_NUMBER}-$COMMIT_SHA_SHORT

1089a2a035a9011aadfb8eeaf0e6d6f19ada219e
Arek Rus <57007917+r-arek@users.noreply.github.com>
2023-06-28 14:09:34 +0200

    Update docker-build.sh

--- docker-build.sh
+++ docker-build.sh
@@ -11,16 +11,10 @@ APP_NAME=$(cut -d "/" -f 2 <<< $K8S_APP_REPO)
 ENV=$(cut -d "-" -f 3 <<< $DEPLOYMENT_NAME)
 COMMIT_SHA_SHORT=$(git rev-parse --short HEAD 2>/dev/null)
 DOCKER_IMAGE_PATH="gcr.io/${GCE_PROJECT}/${ENV}/${APP_NAME}"
-DOCKER_IMAGE_TAG=$(gcloud container images list-tags ${DOCKER_IMAGE_PATH} --filter="tags=${COMMIT_SHA_SHORT}" --format=json)
 
-if [[ "${DOCKER_IMAGE_TAG}" == "[]" ]]; then
-  echo "Tag doesn't exist in gcr.io. Building image..."
-  #gcloud docker -- pull $DOCKER_IMAGE_PATH:latest || true
-  docker build . --build-arg bundle_gems__contribsys__com --cache-from $DOCKER_IMAGE_PATH:latest -t $DOCKER_IMAGE_PATH:build-${TRAVIS_BUILD_NUMBER}-$COMMIT_SHA_SHORT
-  docker tag $DOCKER_IMAGE_PATH:build-${TRAVIS_BUILD_NUMBER}-$COMMIT_SHA_SHORT $DOCKER_IMAGE_PATH:latest
-  gcloud docker -- push $DOCKER_IMAGE_PATH:build-${TRAVIS_BUILD_NUMBER}-$COMMIT_SHA_SHORT
-  gcloud docker -- push $DOCKER_IMAGE_PATH:latest
-else
-  echo "Image already exist in gcr.io"
-  exit 0
-fi
+echo "Building image..."
+#gcloud docker -- pull $DOCKER_IMAGE_PATH:latest || true
+docker build . --build-arg bundle_gems__contribsys__com --cache-from $DOCKER_IMAGE_PATH:latest -t $DOCKER_IMAGE_PATH:build-${TRAVIS_BUILD_NUMBER}-$COMMIT_SHA_SHORT
+docker tag $DOCKER_IMAGE_PATH:build-${TRAVIS_BUILD_NUMBER}-$COMMIT_SHA_SHORT $DOCKER_IMAGE_PATH:latest
+gcloud docker -- push $DOCKER_IMAGE_PATH:build-${TRAVIS_BUILD_NUMBER}-$COMMIT_SHA_SHORT
+gcloud docker -- push $DOCKER_IMAGE_PATH:latest

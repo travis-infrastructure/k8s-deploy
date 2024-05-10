e47ec7ddab796c072c9118b3b37e9a593c35939d
Konrad Wrzos <konrad@travis-ci.org>
2024-05-10 15:45:04 +0200

    Adjusting docker build for travis-web

--- docker-build.sh
+++ docker-build.sh
@@ -15,7 +15,11 @@ DOCKER_IMAGE_TAG=$(gcloud container images list-tags ${DOCKER_IMAGE_PATH} --filt
 if [[ "${DOCKER_IMAGE_TAG}" == "[]" ]]; then
   echo "Tag doesn't exist in gcr.io. Building image..."
   gcloud docker -- pull $DOCKER_IMAGE_PATH:latest || true
-  docker build . --build-arg bundle_gems__contribsys__com --cache-from $DOCKER_IMAGE_PATH:latest -t $DOCKER_IMAGE_PATH:$COMMIT_SHA_SHORT
+  if [[ "${APP_NAME}" == "travis-web" ]]; then
+    docker build . --secret id=GITHUB_PERSONAL_TOKEN,env=GITHUB_PERSONAL_TOKEN --build-arg bundle_gems__contribsys__com --cache-from $DOCKER_IMAGE_PATH:latest -t $DOCKER_IMAGE_PATH:$COMMIT_SHA_SHORT
+  else
+    docker build . --build-arg bundle_gems__contribsys__com --cache-from $DOCKER_IMAGE_PATH:latest -t $DOCKER_IMAGE_PATH:$COMMIT_SHA_SHORT
+  fi
   docker tag $DOCKER_IMAGE_PATH:$COMMIT_SHA_SHORT $DOCKER_IMAGE_PATH:latest
   gcloud docker -- push $DOCKER_IMAGE_PATH:$COMMIT_SHA_SHORT
   gcloud docker -- push $DOCKER_IMAGE_PATH:latest

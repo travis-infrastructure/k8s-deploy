5b7a85cce02d86cfadf0e53723f0afe7f247085e
Konrad Wrzos <84381342+smokodon@users.noreply.github.com>
2024-05-10 15:58:09 +0200

    Update docker build script to work with travis-web private repository

--- docker-build.sh
+++ docker-build.sh
@@ -14,7 +14,11 @@ DOCKER_IMAGE_PATH="gcr.io/${GCE_PROJECT}/${ENV}/${APP_NAME}"
 
 echo "Building image..."
 gcloud docker -- pull $DOCKER_IMAGE_PATH:latest || true
-docker build . --build-arg bundle_gems__contribsys__com --cache-from $DOCKER_IMAGE_PATH:latest -t $DOCKER_IMAGE_PATH:build-${TRAVIS_BUILD_NUMBER}-$COMMIT_SHA_SHORT
+if [[ "${APP_NAME}" == "travis-web" ]]; then
+  docker build . --secret id=GITHUB_PERSONAL_TOKEN,env=GITHUB_PERSONAL_TOKEN --build-arg bundle_gems__contribsys__com --cache-from $DOCKER_IMAGE_PATH:latest -t $DOCKER_IMAGE_PATH:build-${TRAVIS_BUILD_NUMBER}-$COMMIT_SHA_SHORT
+else
+  docker build . --build-arg bundle_gems__contribsys__com --cache-from $DOCKER_IMAGE_PATH:latest -t $DOCKER_IMAGE_PATH:build-${TRAVIS_BUILD_NUMBER}-$COMMIT_SHA_SHORT
+fi
 docker tag $DOCKER_IMAGE_PATH:build-${TRAVIS_BUILD_NUMBER}-$COMMIT_SHA_SHORT $DOCKER_IMAGE_PATH:latest
 gcloud docker -- push $DOCKER_IMAGE_PATH:build-${TRAVIS_BUILD_NUMBER}-$COMMIT_SHA_SHORT
 gcloud docker -- push $DOCKER_IMAGE_PATH:latest

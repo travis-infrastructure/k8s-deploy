fe52cd3b1c0fe016b9983ac4bff5a25f66c04ccc
Dominik <dominik.alberski@gmail.com>
2024-08-07 11:18:45 +0200

    Enable AIDA_URL variable for build

--- docker-build.sh
+++ docker-build.sh
@@ -15,7 +15,7 @@ DOCKER_IMAGE_PATH="gcr.io/${GCE_PROJECT}/${ENV}/${APP_NAME}"
 echo "Building image..."
 gcloud docker -- pull $DOCKER_IMAGE_PATH:latest || true
 if [[ "${APP_NAME}" == "travis-web" ]]; then
-  docker build . --secret id=GITHUB_PERSONAL_TOKEN,env=GITHUB_PERSONAL_TOKEN --build-arg bundle_gems__contribsys__com --cache-from $DOCKER_IMAGE_PATH:latest -t $DOCKER_IMAGE_PATH:build-${TRAVIS_BUILD_NUMBER}-$COMMIT_SHA_SHORT
+  docker build . --secret id=GITHUB_PERSONAL_TOKEN,env=GITHUB_PERSONAL_TOKEN --secret id=AIDA_URL,env=AIDA_URL --build-arg bundle_gems__contribsys__com --cache-from $DOCKER_IMAGE_PATH:latest -t $DOCKER_IMAGE_PATH:build-${TRAVIS_BUILD_NUMBER}-$COMMIT_SHA_SHORT
 else
   docker build . --build-arg bundle_gems__contribsys__com --cache-from $DOCKER_IMAGE_PATH:latest -t $DOCKER_IMAGE_PATH:build-${TRAVIS_BUILD_NUMBER}-$COMMIT_SHA_SHORT
 fi
